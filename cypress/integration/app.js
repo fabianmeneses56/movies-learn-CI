0bdd92741b5d4e0389f1c69582237a2e2bc68ef5
fabianmeneses56 <edison.meneses@uao.edu.co>
2022-05-05 13:49:37 -0500

    add E2E Test to workflow

---
+++ cypress/integration/app.js
@@ -0,0 +1,9 @@
+/* global cy */
+
+describe('Movies', function () {
+  it('frontpage can be opened', function () {
+    cy.visit('http://localhost:3000')
+    cy.contains('Buscar')
+    cy.contains('Buscador de películas')
+  })
+})
